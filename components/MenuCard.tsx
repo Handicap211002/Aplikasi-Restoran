146edce471cecfc4f08ff657401db0b231fb0db6
Handicap <deanhandicap@gmail.com>
2025-07-27 09:11:20 +0700

    test12345678

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -22,7 +22,7 @@ export function MenuCard({ item, onAdd }: Props) {
       initial={{ opacity: 0, y: 40 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{
-        duration: 0.8, // lebih lambat & smooth
+        duration: 0.1, // lebih lambat & smooth
         ease: [0.25, 0.1, 0.25, 1], // cubic bezier klasik
       }}
       viewport={{ once: true, amount: 0.3 }}
